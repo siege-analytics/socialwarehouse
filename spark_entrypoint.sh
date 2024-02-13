9840b7a188e263ab8eb645e099a5528f6843f311
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-02-13 13:41:48 -0600

    1. Progress commit - GDAL is building, trying to get sdkman to run to add JVM things

--- spark_entrypoint.sh
+++ spark_entrypoint.sh
@@ -7,12 +7,13 @@ echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
   start-master.sh -p 7077
-elif:
-  [ "$SPARK_WORKLOAD" == "worker" ];
+fi
+if [ "$SPARK_WORKLOAD" == "worker" ];
 then
   start-worker.sh spark://spark-master:7077
-elif:
-  [ "$SPARK_WORKLOAD" == "history" ];
+fi
+if
+[ "$SPARK_WORKLOAD" == "history" ]
 then
   start-history-server.sh
 fi

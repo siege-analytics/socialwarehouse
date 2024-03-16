b18be15771ae33106135adfcee4907d163cd8070
Steve Blackmon <sblackmon@apache.org>
2024-03-16 15:20:35 -0500

    fix ucar issue (ish)
    Signed-off-by: Steve Blackmon <sblackmon@apache.org>

--- code/bash_scripts/misc.sh
+++ code/bash_scripts/misc.sh
@@ -1,1 +1,5 @@
 export SPARK_HOME=~/Tools/spark-3.4.2-bin-hadoop3-scala2.13
+
+sdk use maven 3.9.3
+
+mvn -U -DremoteRepositories=https://artifacts.unidata.ucar.edu/content/repositories/unidata-releases/ org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=edu.ucar:cdm-core:5.5.3

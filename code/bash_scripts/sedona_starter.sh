ef4114e497bf524729478eb1511f339a555ae3b3
Steve Blackmon <sblackmon@apache.org>
2024-04-12 16:29:29 -0500

    addl config
    Signed-off-by: Steve Blackmon <sblackmon@apache.org>

--- code/bash_scripts/sedona_starter.sh
+++ code/bash_scripts/sedona_starter.sh
@@ -1,3 +1,10 @@
-bin/spark-shell --master local[*] \
+#bin/spark-shell --master local[*] \
+${SPARK_HOME}/bin/spark-shell --master spark://10.10.0.100:7077 \
+  --deploy-mode client \
   --packages org.apache.sedona:sedona-spark-shaded-3.4_2.13:1.5.1,org.datasyslab:geotools-wrapper:1.5.1-28.2,edu.ucar:cdm-core:5.5.3 \
-  --properties-file ./spark-defaults.properties
+  --properties-file ./spark-client-defaults.properties
+#  --driver-memory 2g \
+#  --executor-memory 2g \
+#  --num-executors 2 \
+#  --executor-cores 4 \
+#  --total-executor-cores 8 \
